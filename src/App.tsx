fdd1f0846e8ac4af5a1498d17a9cdb6079610b85
maia <matteusmaia889@gmail.com>
2024-04-24 17:09:16 -0300

    feat: added helmets to the doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./global.css";
 import { router } from "./routes";
 import { RouterProvider } from "react-router-dom";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
