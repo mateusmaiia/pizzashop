5b75f2e1bd28be1e114c8d10b35c391781228d5c
maia <matteusmaia889@gmail.com>
2024-04-26 17:12:12 -0300

    feat: themeProvider component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { Separator } from './ui/separator'
 import { NavLik } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 
 export function Header(){
     
@@ -20,6 +21,10 @@ export function Header(){
                         Pedidos
                     </NavLik>
                 </nav>
+
+                <div className='ml-auto flex items-center gap-2'>
+                    <ThemeToggle/>
+                </div>
             </div>
         </div>
     )
