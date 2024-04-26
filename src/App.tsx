47581a89348f4d67f59c0cf2c652216c2fa614ae
maia <matteusmaia889@gmail.com>
2024-04-26 14:06:50 -0300

    feat: Sonner | Toast    Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
+import { Toaster } from 'sonner'
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster />
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import {useForm} from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 const signInForm = z.object({
@@ -16,7 +17,9 @@ export function SignIn() {
   const {register,  handleSubmit, formState: {isSubmitting}} = useForm()
 
   async function handleSignIn(data: signInForm){
-    
+    console.log(data)
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    toast.success("Enviamos um link de confirmação no seu e-mail")
   }
 
   return (
