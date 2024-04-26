5b45a5137cbb3778be903d52249f8f374ce6501f
maia <matteusmaia889@gmail.com>
2024-04-26 18:00:53 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export function Header(){
     
     return(
         <div className='border-b'>
-            <div className='flex h-16 items-center gap-6 px-6'>
+            <div className='flex  h-16 items-center gap-6 px-6'>
                 <Pizza className='h-6 w-6'/>
                 <Separator orientation='vertical' className='h-6'/>
 
