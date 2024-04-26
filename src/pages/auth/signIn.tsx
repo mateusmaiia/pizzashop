bcdcb098aa768b310af2a345aedee24a3dfbdf14
maia <matteusmaia889@gmail.com>
2024-04-26 15:00:16 -0300

    feat: add LInk from RRT

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -3,11 +3,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import {useForm} from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
 const signInForm = z.object({
-  email: z.string().email()
+
+  email: z.string().email(),
 })
 
 type signInForm = z.infer<typeof signInForm>
@@ -35,6 +37,11 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild variant="ghost" className='absolute right-8 top-8'>
+            <Link to="/sign-up" className='k'>
+                Novo estabelecimento
+            </Link>
+        </Button>
         <div className="w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -45,9 +52,9 @@ export function SignIn() {
             </p>
           </div>
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+            <div className='space-y-2'>
+              <Label htmlFor='email'>Seu e-mail</Label>
+              <Input id='email' type='email' {...register("email")}/>
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit" {...register("submit")}>
