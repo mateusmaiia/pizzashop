a3cc046a9cd33e3072cc6fc743969de818d5ec22
maia <matteusmaia889@gmail.com>
2024-04-30 14:29:11 -0300

    feat: added tableRows

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,6 +1,7 @@
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { Table } from 'lucide-react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
 export function Orders(){
@@ -29,6 +30,46 @@ export function Orders(){
                                 <TableHead></TableHead>
                             </TableRow>
                         </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <Button variant="outline" size="xs">
+                                        <Search className='h-3 w-3'/>
+                                        <span className='sr-only'>Detalhe do pedido</span>
+                                    </Button>
+                                </TableCell>
+                                <TableCell className='font-mono text-xs font-medium'>
+                                    89439kjde19q0k1
+                                </TableCell>
+                                <TableCell className='text-muted-foreground'>
+                                    há 15 minutos
+                                </TableCell>
+                                <TableCell>
+                                    <div className='flex items-center gap-2'>
+                                        <span className='h-2 w-2 rounded-full bg-slate-400'></span>
+                                        <span className='font-medium text-muted-foreground'>Pendente</span>
+                                    </div>
+                                </TableCell>
+                                <TableCell className='font-medium'>
+                                    Matteus Schell Fernandes
+                                </TableCell>
+                                <TableCell className='font-medium'>
+                                    R$ 149,98
+                                </TableCell>
+                                <TableCell className=''>
+                                    <Button variant="outline" size="xs">
+                                        <ArrowRight className='mr-2 h-3 w-3'/>
+                                        Aprovar
+                                    </Button>
+                                </TableCell>  
+                                <TableCell className=''>
+                                    <Button variant="ghost" size="xs">
+                                        <X className='mr-2 h-3 w-3'/>
+                                        Cancelar
+                                    </Button>
+                                </TableCell>    
+                            </TableRow>
+                        </TableBody>
                     </Table>
                 </div>
             </div>
