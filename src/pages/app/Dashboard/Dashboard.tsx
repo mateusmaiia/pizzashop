ca7bcab41c0a72c2369219b5611b14dc886af3a9
Maia <matteusmaia889@gmail.com>
2024-05-01 19:26:11 -0300

    feat: CardContent dashboard

--- src/pages/app/Dashboard/Dashboard.tsx
+++ src/pages/app/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DollarSign } from "lucide-react";
 import { Helmet } from "react-helmet-async";
 
@@ -17,6 +17,19 @@ export function Dashboard() {
               </CardTitle>
               <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
+            <CardContent className="space-y-1">
+              <span className="text-2xl font-bold tracking-tight">
+                R$1248,00
+              </span>
+              <p>
+                <p className="text-xs  text-muted-foreground">
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    2%
+                  </span>{" "}
+                  {"  "}Em relação ao mês passado
+                </p>
+              </p>
+            </CardContent>
           </Card>
         </div>
       </div>
