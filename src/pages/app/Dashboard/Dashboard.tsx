0f9357b30c949cf5b1eecb85d091a1d63e10c120
maia <matteusmaia889@gmail.com>
2024-05-02 14:34:51 -0300

    feat: Comp Card day orders amount

--- src/pages/app/Dashboard/Dashboard.tsx
+++ src/pages/app/Dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from './month-revenue-card'
+import { MonthOrdersAmounthCard } from './month-orders-amount-card'
+import { DayOrdersAmounthCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmounthCard } from './month-canceled-orders-amount-card'
 
 export function Dashboard() {
   return (
@@ -10,27 +12,10 @@ export function Dashboard() {
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
-          <Card>
-            <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-base font-semibold">
-                Receita total (mês)
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent className="space-y-1">
-              <span className="text-2xl font-bold tracking-tight">
-                R$1248,00
-              </span>
-              <p>
-                <p className="text-xs  text-muted-foreground">
-                  <span className="text-emerald-500 dark:text-emerald-400">
-                    2%
-                  </span>{" "}
-                  {"  "}Em relação ao mês passado
-                </p>
-              </p>
-            </CardContent>
-          </Card>
+          <MonthRevenueCard />
+          <MonthOrdersAmounthCard />
+          <DayOrdersAmounthCard />
+          <MonthCanceledOrdersAmounthCard />
         </div>
       </div>
     </>
