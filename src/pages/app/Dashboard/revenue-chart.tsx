1eb23ef813654dc2078a07487d2d467ba9bf1234
mateusmaiia <matteusmaia889@gmail.com>
2024-05-14 02:53:22 -0300

    .

--- src/pages/app/Dashboard/revenue-chart.tsx
+++ src/pages/app/Dashboard/revenue-chart.tsx
@@ -1,70 +1,69 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-import colors from 'tailwindcss/colors'
+import colors from "tailwindcss/colors";
 
 import {
-    ResponsiveContainer,
-    LineChart,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Line,
-    Tooltip
-} from 'recharts'  
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+} from "recharts";
 
 const data = [
-    {data: '10/12', revenue: 1200},
-    {data: '12/12', revenue: 800},
-    {data: '13/12', revenue: 900},
-    {data: '14/12', revenue: 400},
-    {data: '15/12', revenue: 2300},
-    {data: '16/12', revenue: 800},
-    {data: '17/12', revenue: 640},
-]
+  { data: "10/12", revenue: 1200 },
+  { data: "12/12", revenue: 800 },
+  { data: "13/12", revenue: 900 },
+  { data: "14/12", revenue: 400 },
+  { data: "15/12", revenue: 2300 },
+  { data: "16/12", revenue: 800 },
+  { data: "17/12", revenue: 640 },
+];
 
-export function RevenueChart(){
-    return(
-        <Card className='col-span-6'>
-            <CardHeader className='flex-row items-center justify-between gap-8'>
-                <div className="space-y-1 ">
-                    <CardTitle>
-                        Receita no período 
-                    </CardTitle>
-                    <CardDescription> Receita diária no período</CardDescription>
-                </div>
-            </CardHeader>
-            <CardContent>
-              <ResponsiveContainer width="100%" height={240}>
-                <LineChart data={data} style={{fontSize: 12}}>
-                    <XAxis 
-                        dataKey="data"
-                        tickLine={false}
-                        axisLine={false}
-                        dy={16}
-                    />
-                    <YAxis 
-                        stroke="#888"
-                        axisLine={false}
-                        tickLine={false}
-                        width={80}
-                        tickFormatter={(value: number) => 
-                            value.toLocaleString("pt-BR", {
-                                style: 'currency',
-                                currency: 'BRL'
-                            })
-                        }
-                    />
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between gap-8">
+        <div className="space-y-1 ">
+          <CardTitle>Receita no período</CardTitle>
+          <CardDescription> Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              }
+            />
 
-                    <CartesianGrid 
-                        vertical={false}
-                        className='stroke-muted'
-                    />
+            <CartesianGrid vertical={false} className="stroke-muted" />
 
-                    <Line type="linear" strokeWidth={2} dataKey="revenue" stroke={colors.violet['500']}/>
-                
-                </LineChart>   
-              </ResponsiveContainer>  
-            </CardContent>
-        </Card>
-    )
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet["500"]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
 }
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,53 +1,47 @@
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogTrigger } from '@/components/ui/dialog'
-import { TableCell, TableRow } from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
-import { OrderDetails } from './order-details'
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
-export function OrderTableRow(){
-    return(
-        <TableRow >
-            <TableCell>
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button variant="outline" size="xs">
-                            <Search className='h-3 w-3'/>
-                            <span className='sr-only'>Detalhe do pedido</span>
-                        </Button>
-                    </DialogTrigger>
-                    <OrderDetails />
-                </Dialog>    
-            </TableCell>
-            <TableCell className='font-mono text-xs font-medium'>
-                89439kjde19q0k1
-            </TableCell>
-            <TableCell className='text-muted-foreground'>
-                há 15 minutos
-            </TableCell>
-            <TableCell>
-                <div className='flex items-center gap-2'>
-                    <span className='h-2 w-2 rounded-full bg-slate-400'></span>
-                    <span className='font-medium text-muted-foreground'>Pendente</span>
-                </div>
-            </TableCell>
-            <TableCell className='font-medium'>
-                Matteus Schell Fernandes
-            </TableCell>
-            <TableCell className='font-medium'>
-                R$ 149,98
-            </TableCell>
-            <TableCell className=''>
-                <Button variant="outline" size="xs">
-                    <ArrowRight className='mr-2 h-3 w-3'/>
-                    Aprovar
-                </Button>
-            </TableCell>  
-            <TableCell className=''>
-                <Button variant="ghost" size="xs">
-                    <X className='mr-2 h-3 w-3'/>
-                    Cancelar
-                </Button>
-            </TableCell>    
-        </TableRow>
-    )
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhe do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        89439kjde19q0k1
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Matteus Schell Fernandes</TableCell>
+      <TableCell className="font-medium">R$ 149,98</TableCell>
+      <TableCell className="">
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell className="">
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
 }
