f786cf7b68b1f001b18d2bf0be41d725475e76cd
maia <matteusmaia889@gmail.com>
2024-05-23 17:55:07 -0300

    faet: dialogContent header and footer

--- frontend/src/components/store-profile-trigger.tsx
+++ frontend/src/components/store-profile-trigger.tsx
@@ -1,9 +1,19 @@
-import { DialogContent } from './ui/dialog'
+import { Button } from './ui/button'
+import { DialogContent, DialogHeader, DialogDescription, DialogTitle, DialogFooter } from './ui/dialog'
 
 export function StoreProfileDialog(){
     return(
         <DialogContent>
-            Teste
+            <DialogHeader>
+                <DialogTitle>Perfil da loja</DialogTitle>
+                <DialogDescription>
+                    Atualize as informações do seu estabelecimento visíveis ao seu cliente
+                </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+                <Button variant="ghost" type='button'>Cancelar</Button>
+                <Button type='submit' variant='success'>Salvar</Button>
+            </DialogFooter>
         </DialogContent>
     )
 }
