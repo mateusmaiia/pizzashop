29bc4a62ae7ca41338ce6c1a03773e614096851b
maia <matteusmaia889@gmail.com>
2024-05-23 15:50:05 -0300

    update: created frontend and backend folders to organization

--- frontend/src/components/account-menu.tsx
+++ frontend/src/components/account-menu.tsx
@@ -2,23 +2,36 @@ import { Building, ChevronDown, LogOut } from 'lucide-react'
 import { Button } from './ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 
 export function AccountMenu(){
 
+    const {data: profile} = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    })
+    // agora se eu tiver outra requisição com esse nome de queryKey, ela não vai fazer essa requisição de NavigationOff, vai pegar do cache para evitar duplicar a requisição
 
+    const {data: managedRestaurant} = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant
+    })
+    
     return(
         <DropdownMenu>
             {/* //asChild --> passa todas as funcionalidades e propriedades para o componente logo abaixo dele */}
             <DropdownMenuTrigger asChild>
                 <Button variant='outline' className='flex items-center gap-2 select-none'>
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDown />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align='end' className='w-56'>
                 <DropdownMenuLabel className='flex flex-col'>
-                    <span>Mateus Maia</span>
-                    <span className='text-xs font-normal text-muted-foreground'>mateusssmaia@gmail.com</span>
+                    <span>{profile?.name}</span>
+                    <span className='text-xs font-normal text-muted-foreground'>{profile?.email}</span>
                 </DropdownMenuLabel>
 
                 <DropdownMenuSeparator/>
--- frontend/src/pages/auth/signIn.tsx
+++ frontend/src/pages/auth/signIn.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import {useForm} from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 import {useMutation} from '@tanstack/react-query'
@@ -18,11 +18,18 @@ type SignInForm = z.infer<typeof signInForm>
 
 
 export function SignIn() {
+
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit, 
     formState: {isSubmitting}
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? ''
+    }
+  })
 
   const {mutateAsync: authentication} = useMutation({
     mutationFn: signIn,
