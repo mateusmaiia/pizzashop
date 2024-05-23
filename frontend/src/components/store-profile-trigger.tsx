9b5ddb3dcb63e23705c1b00f83e8b9b867020bd2
maia <matteusmaia889@gmail.com>
2024-05-23 18:00:48 -0300

    feat: DialogContent Form

--- frontend/src/components/store-profile-trigger.tsx
+++ frontend/src/components/store-profile-trigger.tsx
@@ -1,5 +1,7 @@
 import { Button } from './ui/button'
 import { DialogContent, DialogHeader, DialogDescription, DialogTitle, DialogFooter } from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
 
 export function StoreProfileDialog(){
     return(
@@ -10,10 +12,26 @@ export function StoreProfileDialog(){
                     Atualize as informações do seu estabelecimento visíveis ao seu cliente
                 </DialogDescription>
             </DialogHeader>
-            <DialogFooter>
-                <Button variant="ghost" type='button'>Cancelar</Button>
-                <Button type='submit' variant='success'>Salvar</Button>
-            </DialogFooter>
+            <form >
+                <div className='space-y-y py-4'>
+                    <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label className='text-right' htmlFor='name'>
+                            Nome
+                        </Label>
+                        <Input className='col-span-3' id='name'/>
+                    </div>
+                    <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label className='text-right' htmlFor='description'>
+                            Descrição
+                        </Label>
+                        <Input className='col-span-3' id='description'/>
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button variant="ghost" type='button'>Cancelar</Button>
+                    <Button type='submit' variant='success'>Salvar</Button>
+                </DialogFooter>
+            </form>
         </DialogContent>
     )
 }
