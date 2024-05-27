a26cf9c7619124f3f8af299d0e657cdea2f2228d
maia <matteusmaia889@gmail.com>
2024-05-27 17:09:09 -0300

    feat: getOrdersQuery interface

--- frontend/src/pages/app/orders/orders.tsx
+++ frontend/src/pages/app/orders/orders.tsx
@@ -11,11 +11,20 @@ import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
 import { useQuery } from '@tanstack/react-query'
 import { getOrders } from '@/api/get-orders'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 export function Orders() {
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const pageIndex = z.coerce
+  .number()
+  .transform((page) => page - 1)
+  .parse(searchParams.get('page') ?? '1')
+
   const {data: result} = useQuery({
     queryKey: ['orders'],
-    queryFn: getOrders
+    queryFn: () => getOrders({pageIndex})
   })
 
   return (
