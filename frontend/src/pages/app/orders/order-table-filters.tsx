34f8dad6067b63e0200ea4ff038d46700aa10f0d
maia <matteusmaia889@gmail.com>
2024-05-28 14:34:02 -0300

    feat: Contoller input

--- frontend/src/pages/app/orders/order-table-filters.tsx
+++ frontend/src/pages/app/orders/order-table-filters.tsx
@@ -3,7 +3,8 @@ import { Input } from '@/components/ui/input'
 import { SelectItem, Select, SelectContent, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Search, X } from 'lucide-react'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 const orderFiltersSchema = z.object({
@@ -12,32 +13,59 @@ const orderFiltersSchema = z.object({
     status: z.string().optional()
 })
 
-type orderFiltersSchema = z.infer<typeof orderFiltersSchema>
-
-const {register} = useForm<orderFiltersSchema>({
-    resolver: zodResolver(orderFiltersSchema)
-})
 
 export function OrderTableFilters(){
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const orderId = searchParams.get('orderId')
+    const customerName = searchParams.get('customerName')
+    const status = searchParams.get('status')
+    
+    type orderFiltersSchema = z.infer<typeof orderFiltersSchema>
+
+    const {register, handleSubmit, control} = useForm<orderFiltersSchema>({
+        resolver: zodResolver(orderFiltersSchema),
+        defaultValues: {
+            orderId: orderId ?? '', 
+            customerName: customerName ?? '', 
+            status: status ?? 'all'
+        }
+    })
+
     return(
         <form className='flex items-center gap-2'>
             <span className='text-sm font-semibold'>Filtros:</span>
             <Input placeholder='ID do pedido' className='h-8 w-auto' {...register('orderId')}/>
             <Input placeholder='Nome do cliente' className='h-8 w-[320px]' {...register('customerName')}/>
-            <Select defaultValue="all">
-                <SelectTrigger  className='h-8 w-[180px]'>
-                    {/* Exibe o texto do item selecionado atualmente pelo usuário */}
-                    <SelectValue />
-                </SelectTrigger>
-                <SelectContent >
-                    <SelectItem value='all'>Todos status</SelectItem>
-                    <SelectItem value='pending'>Pendente</SelectItem>
-                    <SelectItem value='canceled'>Cancelado</SelectItem>
-                    <SelectItem value='processing'>Processando</SelectItem>
-                    <SelectItem value='delivering'>Em preparo</SelectItem>
-                    <SelectItem value='delivered'>Entregue</SelectItem>
-                </SelectContent>
-            </Select>
+            <Controller 
+                name='status'
+                control={control}
+                render={({field: {name, onBlur, onChange, ref, value, disabled}}) => {
+                    return(
+                        <Select 
+                            name={name}
+                            onValueChange={onChange}
+                            value={value}
+                            disabled={disabled}
+                            defaultValue="all"
+                        >
+                            <SelectTrigger  className='h-8 w-[180px]'>
+                                {/* Exibe o texto do item selecionado atualmente pelo usuário */}
+                                <SelectValue />
+                            </SelectTrigger>
+                            <SelectContent >
+                                <SelectItem value='all'>Todos status</SelectItem>
+                                <SelectItem value='pending'>Pendente</SelectItem>
+                                <SelectItem value='canceled'>Cancelado</SelectItem>
+                                <SelectItem value='processing'>Processando</SelectItem>
+                                <SelectItem value='delivering'>Em preparo</SelectItem>
+                                <SelectItem value='delivered'>Entregue</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    )
+                }}
+            />
 
             <Button type='submit' variant="secondary" size="xs">
                 <Search className='mr-2 h-4 w-4'/>
