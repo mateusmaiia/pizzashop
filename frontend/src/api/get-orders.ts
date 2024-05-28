0d6692e4dea9ec6be71a715861759361fe8dcc9f
maia <matteusmaia889@gmail.com>
2024-05-28 15:09:34 -0300

    feat: request type

--- frontend/src/api/get-orders.ts
+++ frontend/src/api/get-orders.ts
@@ -1,7 +1,10 @@
 import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery{
-    pageIndex?: number | null
+    pageIndex?: number | null,
+    orderId?: string | null,
+    customerName?: string | null,
+    status?: string | null,
 }
 
 export interface GetOrdersResponse {
@@ -19,10 +22,13 @@ export interface GetOrdersResponse {
     }
 }
 
-export async function getOrders({pageIndex}:GetOrdersQuery){
+export async function getOrders({pageIndex, customerName, orderId, status}:GetOrdersQuery){
     const response = await api.get<GetOrdersResponse>('/orders', {
         params: {
-            pageIndex
+            pageIndex,
+            customerName,
+            orderId, 
+            status 
         }
     })
 
