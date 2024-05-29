5634d370c7b1ff9764cf2c2f68a9b94e707e8694
maia <matteusmaia889@gmail.com>
2024-05-29 16:35:20 -0300

    feat: query dayOrdersAmount

--- frontend/src/pages/app/Dashboard/day-orders-amount-card.tsx
+++ frontend/src/pages/app/Dashboard/day-orders-amount-card.tsx
@@ -1,7 +1,15 @@
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query'
 import {  Utensils } from 'lucide-react'
 
 export function DayOrdersAmounthCard(){
+
+    const {data: dayOrdersAmount} = useQuery({
+      queryKey: ['metrics','day-orders-amouth'],
+      queryFn: getDayOrdersAmount
+    })
+
     return(
         <Card>
         <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,17 +19,32 @@ export function DayOrdersAmounthCard(){
           <Utensils className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent className="space-y-1">
-          <span className="text-2xl font-bold tracking-tight">
-            12
-          </span>
-          <p>
-            <p className="text-xs  text-muted-foreground">
-              <span className="text-rose-500 dark:text-rose-400">
-                -4%
-              </span>{" "}
-              {"  "}Em relação a ontem
-            </p>
-          </p>
+          {dayOrdersAmount && (
+            <>
+              <span className="text-2xl font-bold tracking-tight">
+                {dayOrdersAmount.amount.toLocaleString('pt-BR')}
+              </span>
+              <p>
+                <p className="text-xs  text-muted-foreground">
+                  {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                    <>
+                      <span className="text-emerald-500 dark:text-emerald-400">
+                        +{dayOrdersAmount.diffFromYesterday}%
+                      </span>{" "}
+                      {"  "}Em relação a ontem
+                    </>
+                  ) : (
+                    <>
+                      <span className="text-rose-500 dark:text-rose-400">
+                        {dayOrdersAmount.diffFromYesterday}%
+                      </span>{" "}
+                      {"  "}Em relação a ontem
+                    </>
+                  )} 
+                </p>
+              </p>
+            </>
+          )}
         </CardContent>
       </Card>
     )
