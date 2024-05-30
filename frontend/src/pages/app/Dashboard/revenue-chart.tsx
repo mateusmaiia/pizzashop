84651eaf9bd7c9200debcef13ade75935fc018da
maia <matteusmaia889@gmail.com>
2024-05-30 15:40:21 -0300

    faet: loarder empty revenueChart

--- frontend/src/pages/app/Dashboard/revenue-chart.tsx
+++ frontend/src/pages/app/Dashboard/revenue-chart.tsx
@@ -26,6 +26,7 @@ import { DateRangePicker } from '@/components/ui/date-ranger-picker'
 import { useMemo, useState } from 'react'
 import { DateRange } from 'react-day-picker'
 import {subDays} from 'date-fns'
+import { Loader2 } from 'lucide-react'
 
 
 export function RevenueChart() {
@@ -68,7 +69,7 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {dailyRevenuePeriod && (
+        {dailyRevenuePeriod ? (
           <ResponsiveContainer width="100%" height={240}>
             <LineChart data={chartData} style={{ fontSize: 12 }}>
               <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
@@ -95,6 +96,10 @@ export function RevenueChart() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ): (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className='h-8 w-8 text-muted-foreground animate-spin' />
+          </div>
         )}
       </CardContent>
     </Card>
