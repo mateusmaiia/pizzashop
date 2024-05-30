e6556f5d868b3b74f917c69e737bc2affb9959f1
maia <matteusmaia889@gmail.com>
2024-05-30 15:20:29 -0300

    feat: props interface and setting params

--- frontend/src/api/get-daily-revenue-in-period.ts
+++ frontend/src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,24 @@
 import { api } from "@/lib/axios";
 
+export interface GetDailyRevenueInPeriodyQuery{
+  from?: Date
+  to?: Date
+}
+
 export type getDailyRevenueInPeriodResponse = {
   date: string;
   receipt: number;
 }[];
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({from, to}: GetDailyRevenueInPeriodyQuery) {
   const response = await api.get<getDailyRevenueInPeriodResponse>(
     "/metrics/daily-receipt-in-period",
+    {
+      params: {
+        from, 
+        to
+      }
+    }
   );
 
   return response.data;
