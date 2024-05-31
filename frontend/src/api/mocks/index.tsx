6ecfe44d4ce17e61760dd3e4c7e8cf1e5cf8e018
maia <matteusmaia889@gmail.com>
2024-05-31 18:06:06 -0300

    faet: import mocks

--- frontend/src/api/mocks/index.tsx
+++ frontend/src/api/mocks/index.tsx
@@ -2,8 +2,20 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 import { signInMock } from './sign-in-mock'
+import { registerRestaurantMock } from './register-resturant-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amout'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock, 
+  registerRestaurantMock, 
+  getDayOrdersAmountMock, 
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock, 
+  getMonthRevenueMock
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
