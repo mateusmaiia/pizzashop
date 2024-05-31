c8932fed2e3f69c7db253a394aea8f63b14938ab
maia <matteusmaia889@gmail.com>
2024-05-31 18:07:10 -0300

    identification

--- frontend/src/pages/app/Dashboard/popular-products-chart.tsx
+++ frontend/src/pages/app/Dashboard/popular-products-chart.tsx
@@ -1,11 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
-import colors from "tailwindcss/colors";
-
-import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
-import { BarChart, Loader2 } from "lucide-react";
 import { useQuery } from '@tanstack/react-query'
-import { getPopularProducts } from '@/api/get-popular-products'
+import { BarChart, Loader2 } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import { getPopularProducts } from '../../../api/get-popular-products'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 const COLORS = [
   colors.sky[500],
@@ -13,21 +12,21 @@ const COLORS = [
   colors.violet[500],
   colors.emerald[500],
   colors.rose[500],
-];
+]
 
 export function PopularProductsChart() {
-
-  const {data: popularProducts } = useQuery({
-    queryKey: ['metrics','popular-products'],
-    queryFn: getPopularProducts
+  const { data: popularProducts } = useQuery({
+    queryKey: ['metrics', 'popular-products'],
+    queryFn: getPopularProducts,
   })
-
 
   return (
     <Card className="col-span-3">
       <CardHeader className="pb-8">
-        <div className="flex items-center justify-between">
-          <CardTitle>Produtor ppulares</CardTitle>
+        <div className="flex items-center justify-between ">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
           <BarChart className="h-4 w-4 text-muted-foreground" />
         </div>
       </CardHeader>
@@ -37,8 +36,8 @@ export function PopularProductsChart() {
             <PieChart style={{ fontSize: 12 }}>
               <Pie
                 data={popularProducts}
-                dataKey="amount"
                 nameKey="product"
+                dataKey="amount"
                 cx="50%"
                 cy="50%"
                 outerRadius={86}
@@ -54,45 +53,47 @@ export function PopularProductsChart() {
                   value,
                   index,
                 }) => {
-                  const RADIAN = Math.PI / 180;
-                  const radius = 12 + innerRadius + (outerRadius - innerRadius);
-                  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-                  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+                  const RADIAN = Math.PI / 180
+                  const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                  const y = cy + radius * Math.sin(-midAngle * RADIAN)
 
                   return (
                     <text
                       x={x}
                       y={y}
                       className="fill-muted-foreground text-xs"
-                      textAnchor={x > cx ? "start" : "end"}
+                      textAnchor={x > cx ? 'start' : 'end'}
                       dominantBaseline="central"
                     >
                       {popularProducts[index].product.length > 12
-                        ? popularProducts[index].product.substring(0, 12).concat("...")
-                        : popularProducts[index].product}{" "}
+                        ? popularProducts[index].product
+                            .substring(0, 12)
+                            .concat('...')
+                        : popularProducts[index].product}{' '}
                       ({value})
                     </text>
-                  );
+                  )
                 }}
               >
-                {popularProducts.map((_, index) => {
+                {popularProducts?.map((_, index) => {
                   return (
                     <Cell
                       key={`cell-${index}`}
                       fill={COLORS[index]}
                       className="stroke-background hover:opacity-80"
                     />
-                  );
+                  )
                 })}
               </Pie>
             </PieChart>
           </ResponsiveContainer>
         ) : (
           <div className="flex h-[240px] w-full items-center justify-center">
-            <Loader2 className='h-8 w-8 text-muted-foreground animate-spin' />
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
           </div>
         )}
       </CardContent>
     </Card>
-  );
+  )
 }
--- frontend/src/pages/app/Dashboard/revenue-chart.tsx
+++ frontend/src/pages/app/Dashboard/revenue-chart.tsx
@@ -1,107 +1,100 @@
+import { useQuery } from '@tanstack/react-query'
+import { subDays } from 'date-fns'
+import { Loader2 } from 'lucide-react'
+import { useMemo, useState } from 'react'
+import { DateRange } from 'react-day-picker'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-
-import colors from "tailwindcss/colors";
-
-import {
-  ResponsiveContainer,
-  LineChart,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Line,
-} from "recharts";
-import { useQuery } from "@tanstack/react-query";
-import {
-  getDailyRevenueInPeriod,
-  getDailyRevenueInPeriodResponse,
-} from "@/api/get-daily-revenue-in-period";
+} from '@/components/ui/card'
+import { DateRangePicker } from '../../../components/ui/date-ranger-picker'
 import { Label } from '@/components/ui/label'
-import { DateRangePicker } from '@/components/ui/date-ranger-picker'
-import { useMemo, useState } from 'react'
-import { DateRange } from 'react-day-picker'
-import {subDays} from 'date-fns'
-import { Loader2 } from 'lucide-react'
-
 
 export function RevenueChart() {
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
     from: subDays(new Date(), 7),
-    to: new Date()
+    to: new Date(),
   })
 
-  const { data: dailyRevenuePeriod } =
-    useQuery<getDailyRevenueInPeriodResponse>({
-      queryKey: ["metrics", "daily-revenue-in-period", dateRange],
-      queryFn: () => getDailyRevenueInPeriod({
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
         from: dateRange?.from,
-        to: dateRange?.to
+        to: dateRange?.to,
       }),
-    });
-    
-    const chartData = useMemo(() => {
-      return dailyRevenuePeriod?.map(chartItem => {
-        return({
-          date: chartItem.date,
-          receipt: chartItem.receipt / 100
-        })
-      }) 
-    }, [dailyRevenuePeriod])
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      }
+    })
+  }, [dailyRevenueInPeriod])
 
   return (
     <Card className="col-span-6">
-      <CardHeader className="flex-row items-center justify-between gap-8">
-        <div className="space-y-1 ">
-          <CardTitle>Receita no período</CardTitle>
-          <CardDescription> Receita diária no período</CardDescription>
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
         </div>
+
         <div className="flex items-center gap-3">
           <Label>Período</Label>
-          <DateRangePicker 
-            date={dateRange}
-            onDateChange={setDateRange}
-          />
+          <DateRangePicker date={dateRange} onDateChange={setDateRange} />
         </div>
       </CardHeader>
       <CardContent>
-        {dailyRevenuePeriod ? (
+        {chartData ? (
           <ResponsiveContainer width="100%" height={240}>
             <LineChart data={chartData} style={{ fontSize: 12 }}>
-              <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
+              <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
               <YAxis
                 stroke="#888"
                 axisLine={false}
                 tickLine={false}
                 width={80}
                 tickFormatter={(value: number) =>
-                  value.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
+                  value.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
                   })
                 }
               />
-
               <CartesianGrid vertical={false} className="stroke-muted" />
-
               <Line
+                stroke={colors.violet[500]}
                 type="linear"
                 strokeWidth={2}
                 dataKey="receipt"
-                stroke={colors.violet["500"]}
               />
             </LineChart>
           </ResponsiveContainer>
-        ): (
+        ) : (
           <div className="flex h-[240px] w-full items-center justify-center">
-            <Loader2 className='h-8 w-8 text-muted-foreground animate-spin' />
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
           </div>
         )}
       </CardContent>
     </Card>
-  );
+  )
 }
