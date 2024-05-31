f3d57511a6ffaf8e485b6309589a63487061b93b
maia <matteusmaia889@gmail.com>
2024-05-31 18:03:59 -0300

    signUp import

--- frontend/src/pages/auth/signUp.tsx
+++ frontend/src/pages/auth/signUp.tsx
@@ -1,4 +1,4 @@
-import { registerRestaurant } from '@/api/register'
+import { registerRestaurant } from '../../api/register-restaurant'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
